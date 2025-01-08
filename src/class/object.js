eee0afac69a19a2f2f7dca300c9071f141ed5aa0
Gerald-Z <g43zhu@uwaterloo.ca>
2025-01-07 23:22:53 -0500

    Moved contact check to class

--- src/class/object.js
+++ src/class/object.js
@@ -8,8 +8,25 @@ export class Box extends THREE.Object3D {
 
         const material = new THREE.MeshBasicMaterial( {color: color });
         const geometry = new THREE.BoxGeometry(width, height, depth)
+        this.material = material
+        this.geometry = geometry
         const item = new THREE.Mesh( geometry, material )
         this.add(item)
+        this.geometry.computeBoundingBox();
+        const box = this.geometry.boundingBox;
+    }
+
+    contact(x, y, z) {
+        this.geometry.computeBoundingBox();
+        const box = this.geometry.boundingBox;
+        return (
+            x < this.position.x + box.min.x || 
+            x > this.position.x + box.max.x ||
+            y < this.position.y + box.min.y ||
+            y > this.position.y + box.max.y ||
+            z < this.position.z + box.min.z ||
+            z > this.position.z + box.max.z 
+        ) ? false : true
     }
 }
 
@@ -20,6 +37,8 @@ export class Sphere extends THREE.Object3D {
 
         const material = new THREE.MeshBasicMaterial( {color: color });
         const geometry = new THREE.SphereGeometry( radius, width_segment, height_segment ); 
+        this.material = material
+        this.geometry = geometry
         const item = new THREE.Mesh( geometry, material )
         this.add(item)
     }
--- src/orb.js
+++ src/orb.js
@@ -4,24 +4,8 @@ import { CONSTANTS } from './const/objects';
 
 const CAMERA_POSITION_Z = 50
 
-const SPHERE_RADIUS = 1
-const SPHERE_WIDTH_SEGMENTS = 32
-const SPHERE_HEIGHT_SEGMENTS = 16
+
 let drop_speed = -1
-
-const BOX_WIDTH = 100
-const BOX_HEIGHT = 10
-const BOX_DEPTH = 5
-const BOX_X = 0
-const BOX_Y = -40
-const BOX_TOP_Y = BOX_Y + BOX_HEIGHT / 2
-
-const TOP_BOX_WIDTH = 100
-const TOP_BOX_HEIGHT = 10
-const TOP_BOX_DEPTH = 5
-const TOP_BOX_X = 0
-const TOP_BOX_Y = 40
-const TOP_BOX_BOTTOM_Y = TOP_BOX_Y - TOP_BOX_HEIGHT / 2
 
 
 const scene = new THREE.Scene()
@@ -31,30 +15,6 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate )
 document.body.appendChild( renderer.domElement )
-
-// Create the falling sphere
-const geometry = new THREE.SphereGeometry( SPHERE_RADIUS, SPHERE_WIDTH_SEGMENTS, SPHERE_HEIGHT_SEGMENTS ); 
-const material = new THREE.MeshBasicMaterial( {color: 'white' })
-const sphere = new THREE.Mesh( geometry, material )
-// scene.add( sphere )
-
-// Create the desk
-const box_geometry = new THREE.BoxGeometry(BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH)
-const box_material = new THREE.MeshBasicMaterial( {color: 0x00ff00 })
-const box = new THREE.Mesh( box_geometry, box_material )
-box.position.x = BOX_X
-box.position.y = BOX_Y
-// scene.add( box )
-
-
-
-// Create the desk
-const top_box_geometry = new THREE.BoxGeometry(TOP_BOX_WIDTH, TOP_BOX_HEIGHT, TOP_BOX_DEPTH)
-const top_box_material = new THREE.MeshBasicMaterial( {color: 'red' })
-const top_box = new THREE.Mesh( top_box_geometry, top_box_material )
-top_box.position.x = TOP_BOX_X
-top_box.position.y = TOP_BOX_Y
-//scene.add( top_box )
 
 
 const objects = {}
@@ -71,20 +31,19 @@ CONSTANTS.forEach((item) => {
 
 
 
-
 camera.position.z = CAMERA_POSITION_Z
 
 function animate() {
    // sphere.rotation.x += 0.01
    const sphere = objects.ball
+    const {x, y, z} = sphere.position;
     sphere.rotation.y += 0.1
-    if (sphere.position.y < BOX_TOP_Y || sphere.position.y > TOP_BOX_BOTTOM_Y) { 
+    if (objects.bottom_box.contact(x, y, z)
+         || objects.top_box.contact(x, y, z)) { 
         drop_speed *= -1
     }
     sphere.position.y += drop_speed
 
-   // console.log(MINIMUM_Y_COORDINATE)
-   // console.log(sphere.position.y)
     renderer.render( scene, camera )
 }
 
